5ee19b90023d173a9441083283312cac3fec1d21
madara-tribe <tsukuba.univer@gmail.com>
2025-08-03 21:25:26 +0900

    update

---
+++ install.sh
@@ -0,0 +1,3 @@
+pip3 install stable-baselines3 opencv-python numpy
+pip3 install 'shimmy>=2.0' tensorboard
+
---
+++ run.sh
@@ -0,0 +1,8 @@
+cd [root]
+python3 -m pip install -e .
+
+# tf board
+tensorboard --logdir logs/ppo_laser_tracker_tensorboard/
+http://localhost:6006/
+
+
